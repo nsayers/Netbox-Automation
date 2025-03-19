07d0b85050ed50869b6f4eb1a9b1afe0dd988f56
nsayers <60269293+nsayers@users.noreply.github.com>
2025-03-19 09:56:19 -0700

    update logging option
    updated to offload debug log to disk

--- xen-automation.pl
+++ xen-automation.pl
@@ -22,7 +22,7 @@ our $netbox_virtualization_url = "virtualization/virtual-machines/";
 
 my ($virtual_cluster, $url, $domain, $user, $pass, $session_response, $client);
 
-my $excluded; # grab skipped info, so we can export it at the end if called.
+my $excluded;
 our $debug = 0;
 
 GetOptions(
@@ -39,6 +39,9 @@ if(!defined($virtual_cluster)){
     HelpMessage();
     die "Error: No cluster defined\n"; 
 }
+
+my $error = 0;
+my $error_output;
 
 my $xserver = XenConfig::get_xenserver($virtual_cluster);
 
@@ -87,19 +90,20 @@ if($xenversion eq '6.5') {
 }
 
 print "$virtual_cluster automation ran at " . localtime . "\n";
+$error_output .= "$virtual_cluster automation ran at " . localtime . "\n";
+
+my $logfile = "/var/log/automation/xen-automation_$virtual_cluster.log";
 
 if(defined($xenLogin)) {
     $client = RPC::XML::Client->new($xenserver);
     $session_response = $client->simple_request($xenLogin, $xenuser, $xenpass);
 } else {
-    print "Undefined version, please escalate to admin\n";
-    exit;
+    die "Undefined version, please escalate to admin\n";
 }
 
 my $session;
 if (defined($session_response) && ref($session_response) eq 'HASH' && $session_response->{Status} eq 'Success') {
     $session = $session_response->{Value};
-    # if($debug) { print "Session OpaqueRef: $session\n"; }
 } else {
     die "Authentication failed! Invalid session response: " . Dumper($session_response);
 }
@@ -153,12 +157,11 @@ foreach my $vm_ref (keys %$vms) {
     }
 
     if($is_snapshot eq '1' || $is_template eq '1') { 
-        if($debug) { print "Found $name_label is a template/snapshot, skipping.\n"; }
-        $excluded .= "VM Name: $name_label, Power State: $power_state, vCPUs: $vcpu_max, Memory: $memory, Template: $is_template, Snapshot: $is_snapshot\n"; 
+        $error_output .= "Found $name_label is a template/snapshot, skipping.\n"; 
         next; 
     }
 
-    if($debug) { print "Checking for $record->{name_label} in netbox ....\n"; }
+    $error_output .= "Checking for $record->{name_label} in netbox ....\n";
 
     if($name_label =~ m/Control domain on host/) { 
         my ($garbage, $node_name) = split(":", $name_label);
@@ -168,13 +171,13 @@ foreach my $vm_ref (keys %$vms) {
 
     $name_label =~ s/\s+//g;
 
-    if($debug) { print "VM Name: $name_label, Power State: $power_state, vCPUs: $vcpu_max, Memory: $memory, Template: $is_template, Snapshot: $is_snapshot\n"; }
+    $error_output .= "VM Name: $name_label, Power State: $power_state, vCPUs: $vcpu_max, Memory: $memory, Template: $is_template, Snapshot: $is_snapshot\n";
 
     ##
     ## Get the disks
     ##
 
-    if($debug) { print "\tDisk assignments:\n"; }
+    $error_output .= "\tDisk assignments:\n";
 
     my $disk_assignments = [];
     my $vbd_refs = $record->{VBDs} // [];
@@ -201,26 +204,26 @@ foreach my $vm_ref (keys %$vms) {
                     my $vdi_size_bytes = $vdi_record->{Value}->{virtual_size} // 0;  
                     $vdi_size_gb = $vdi_size_bytes / (1024**3); 
                 } else {
-                    if($debug) { print "\tsomething went wrong, check $vbd_VDI for issues\n"; }
+                    $error_output .= "\tsomething went wrong, check $vbd_VDI for issues\n";
                     next;
                 }
 
                 push @$disk_assignments, { device => $vbd_device, type => $vbd_type, size_gb => $vdi_size_gb };
 
-                if($debug) { print "\t\tDevice: $vbd_device, Type: $vbd_type, Size: $vdi_size_gb G\n"; }
+                $error_output .= "\t\tDevice: $vbd_device, Type: $vbd_type, Size: $vdi_size_gb G\n";
 
             }
 
 
         }
     } else {
-        if($debug) { print "No VBDs found for VM: $name_label\n"; } 
+        $error_output .= "No VBDs found for VM: $name_label\n";
     }
 
     ##
     ## Get Network settings
     ## 
-    if($debug) { print "\tNetwork Assignments:\n"; }
+    $error_output .= "\tNetwork Assignments:\n";
 
     my $network_assignments = [];
     my $vif_refs = $record->{VIFs} // [];
@@ -239,9 +242,8 @@ foreach my $vm_ref (keys %$vms) {
 
             push @$network_assignments, { mac_address => $mac_address, name => $device, description => $network_ref };
 
-            if($debug) { print "\t\tVIF MAC: $mac_address, Network: $network_ref\n"; }
+            $error_output .= "\t\tVIF MAC: $mac_address, Network: $network_ref\n";
 
-            # Get VM Guest Metrics to find assigned IPs
             my $metrics_ref = $record->{guest_metrics} // "OpaqueRef:NULL";
             if ($metrics_ref ne "OpaqueRef:NULL") {
                 my $metrics = $client->simple_request($xenVMMetrix, $session, $metrics_ref);
@@ -260,7 +262,7 @@ foreach my $vm_ref (keys %$vms) {
                 }
             }
         } else {
-            if($debug) { print "\tFailed to fetch VIF record for $vif_ref\n"; }
+            $error_output .= "\tFailed to fetch VIF record for $vif_ref\n";
         }
     }
 
@@ -285,15 +287,19 @@ foreach my $vm_ref (keys %$vms) {
 
 #if($debug) { print "\nHere is the stuff that we will not be inserting : \n" . $excluded; }
 
-# Logout only if the session was created successfully
 my $logout = $client->simple_request($xenLogOut, $session);
 
+
+if($debug) { print "$error_output\n"; }
+
+open(my $fh, '>>', $logfile) or die "Could not open file '$logfile' $!";
+print $fh "$error_output\n";
+close($fh);
 
 
 sub xenserver_node {
     my ($node_name,$cluster,$site,$status,$client,$netbox_api_url,$netbox_ua) = @_; 
-
-    if($debug) {  print "Checking to see if node $node_name in $cluster cluster exists\n"; }
+    $error_output .= "Checking to see if node $node_name in $cluster cluster exists\n";
     
     my $netbox_dcim_device_url = "dcim/devices/";
 
@@ -307,10 +313,10 @@ sub xenserver_node {
     }
 
     if (exists $netbox_data->{results} && @{$netbox_data->{results}} > 0) {
-        if ($debug) { print "\tNode $node_name exists in NetBox.\n"; }
+        $error_output .= "\tNode $node_name exists in NetBox.\n";
         return 1;
     } else {
-        if($debug) { print "\tNode $node_name does not exist in NetBox, adding ... \n"; }
+        $error_output .= "\tNode $node_name does not exist in NetBox, adding ... \n";
         my $insert_payload = {
             name         => $node_name,
             device_type  => 17,
@@ -327,10 +333,11 @@ sub xenserver_node {
         );
 
         if ($post_response->is_success) {
-            if ($debug) { print "\t\tAdded node $node_name to NetBox.\n"; }
+            $error_output .= "\t\tAdded node $node_name to NetBox.\n";
             return 1;
         } else {
             warn "Failed to add node $node_name: ", $post_response->status_line, "\n";
+            $error++;
             return 0;
         }
     }
@@ -386,20 +393,16 @@ sub update_or_add_vm {
             compare_disks($existing_vm_data->{id}, $vm_data->{disk_assignments});
             compare_interfaces($existing_vm_data->{id}, $vm_data->{networks});
         } else {
-            if($debug) { print "\tNo core changes for VM: $vm_data->{name}\n"; }
+            $error_output .= "\tNo core changes for VM: $vm_data->{name}\n";
             compare_disks($existing_vm_data->{id}, $vm_data->{disk_assignments});
             compare_interfaces($existing_vm_data->{id}, $vm_data->{networks}, $vm_data->{nic_allocations});
         }
     } else {
-        # VM doesn't exist, add it
         add_vm_to_db($vm_data);
-        # print "Added new VM: $vm_data->{name}\n";    # YOU LIE, you are not validating SHIT
-
-        #compare_disks($existing_vm->{key}, $vm_data->{disk_assignments});
     }
 }
 
-# Example of a function that checks if VM data has changed
+
 sub is_vm_data_changed {
     my ($existing_vm, $vm_data) = @_;
     if(!defined($existing_vm) || !defined($vm_data) ) { print "\tis_vm_data_changed being called to compare, but no vm_data being passed to something\n"; return 0;}
@@ -407,22 +410,19 @@ sub is_vm_data_changed {
     my $existing_vm_data = $existing_vm->{$vm_data->{name}};
 
     if (!defined($existing_vm_data)) {
-        if($debug) { print "\tNo existing VM data found for $vm_data->{name}\n"; }
+        $error_output .= "\tNo existing VM data found for $vm_data->{name}\n";
         return 0;  # No existing VM data found for this VM name
     }
 
-    # Compare existing VM data with new data
     return lc($existing_vm_data->{status}) ne lc($vm_data->{status}) ||  # Compare power state
            $existing_vm_data->{memory} ne $vm_data->{memory} ||  # Compare memory
            $existing_vm_data->{vcpus} ne $vm_data->{vcpus};
 }
 
-# Compare networks between the existing VM and the new data
 sub compare_networks {
     my ($existing_networks, $new_networks) = @_;
     if(!defined($existing_networks) || !defined($new_networks) ) { print "\tcompare_networks being called to compare VM's, but no vm_data being passed\n"; }
     
-    # Logic to compare network assignments (MAC address, network) between existing and new networks
     return 1 if scalar(@$existing_networks) != scalar(@$new_networks);  # Different number of networks
     for my $i (0 .. $#{$existing_networks}) {
         return 1 if $existing_networks->[$i]->{mac_address} ne $new_networks->[$i]->{mac_address} ||
@@ -431,7 +431,6 @@ sub compare_networks {
     return 0;
 }
 
-# Subroutine to add a new VM to the DB or NetBox
 sub add_vm_to_db {
     my ($vm_data) = @_;
 
@@ -441,7 +440,7 @@ sub add_vm_to_db {
 
     my $vm_name = $vm_data->{name};
 
-    if($debug) { print "\tAdding VM $vm_name to NetBox...\n"; }
+    $error_output .= "\tAdding VM $vm_name to NetBox...\n";
 
     my $insert_payload = {
         name    => $vm_name,
@@ -463,17 +462,18 @@ sub add_vm_to_db {
         if($debug) { print "\t\tAdded VM $vm_name to NetBox.\n"; }
     } else {
         warn "Failed to add VM $vm_name: ", $post_response->status_line, "\n";
+        $error++;
+        $error_output .= "Failed to add VM $vm_name: ", $post_response->status_line, "\n"
     }
 }
 
-# Subroutine to update an existing VM in the DB or NetBox
 sub update_vm_in_db {
     my ($vmid, $vm_data) = @_;
 
     if(!defined($vmid)) { print "being called to update, but no vmid being passed\n"; return; }
     # Update the VM data in the DB or API (e.g., NetBox)
     # This is a placeholder, replace with actual logic to update the VM.
-    if($debug) { print "\t\tUpdating VM with UUID: $vmid\n\n\n"; }
+    $error_output .= "\t\tUpdating VM with UUID: $vmid\n\n\n";
 }
 
 
@@ -506,14 +506,14 @@ sub compare_disks {
             # Compare based on disk name and size
             if ($netbox_disk->{name} eq $new_disk_name && $netbox_disk->{size} == $new_disk_size) {
                 $found_match = 1;
-                if($debug) { print "\t\tMatch found for disk $new_disk_name\n"; }
+                $error_output .= "\t\tMatch found for disk $new_disk_name\n";
                 last;
             }
         }
 
         # If no match found, update or add the disk
         unless ($found_match) {
-            if($debug) { print "\t\tNo match found for disk $new_disk_name. Adding to NetBox...\n"; }
+            $error_output .= "\t\tNo match found for disk $new_disk_name. Adding to NetBox...\n"; 
             # Update or add new disk logic here.
             my $disk_data = {
                 virtual_machine    => $netbox_id,
@@ -529,19 +529,18 @@ sub compare_disks {
             );
             
             if ($post_response->is_success) {
-                if($debug) { print "\t\t\tAdded disk $new_disk_name to $netbox_id in NetBox.\n"; }
+                $error_output .= "\t\t\tAdded disk $new_disk_name to $netbox_id in NetBox.\n"; 
             } else {
-                warn "Failed to add Disk $new_disk_name: ", $post_response->status_line, "\n";
+                $error++; 
+                $error_output .= "Failed to add Disk $new_disk_name: " . $post_response->status_line;
             }
         }
     }
 
-    # Now remove any disks from NetBox that were not found in the new disks array
     foreach my $netbox_disk_id (keys %netbox_disks) {
         my $netbox_disk = $netbox_disks{$netbox_disk_id};
         my $found_match = 0;
 
-        # Check if the disk is in the new disks array
         foreach my $new_disk (@$new_disks) {
             my $new_disk_name = $new_disk->{device};
             my $new_disk_size = $new_disk->{size_gb} // 0;
@@ -551,12 +550,11 @@ sub compare_disks {
             }
         }
 
-        # If the disk is not found in the new disks array, remove it
         unless ($found_match) {
-            if($debug) { print "\t\t\tDisk $netbox_disk->{name} not found in the new disks. Removing from NetBox...\n"; }
+            $error_output .= "\t\t\tDisk $netbox_disk->{name} not found in the new disks. Removing from NetBox...\n"; 
             my $delete_response = $netbox_ua->delete("$netbox_api_url$netbox_disk_url$netbox_disk_id");
             die "Failed to delete disk from NetBox: ", $delete_response->status_line unless $delete_response->is_success;
-            if($debug) { print "\t\t\t\tDisk $netbox_disk->{name} successfully removed from NetBox.\n"; }
+            $error_output .= "\t\t\t\tDisk $netbox_disk->{name} successfully removed from NetBox.\n"; 
         }
     }
 }
@@ -594,7 +592,7 @@ sub compare_interfaces {
         }
 
         unless ($found_match) {
-            if($debug) { print "\t\tNo match found for interface: " . $new_interface->{name} . ", VM : " . $netbox_id . " . Adding to NetBox...\n"; }
+            $error_output .= "\t\tNo match found for interface: " . $new_interface->{name} . ", VM : " . $netbox_id . " . Adding to NetBox...\n"; 
 
             my $iface_data = {
                 virtual_machine => $netbox_id,
@@ -611,13 +609,11 @@ sub compare_interfaces {
                 Content        => $json_payload
             );
 
-            # print "curl -X POST \"$netbox_api_url$netbox_virt_int_url\" -H \"Content-Type: application/json\" -d $json_payload";
-
             if ($post_response->is_success) {
-                if($debug) { print "\t\t\tAdded interface " . $new_interface->{mac_address} . " to NetBox.\n"; }
+                $error_output .= "\t\t\tAdded interface " . $new_interface->{mac_address} . " to NetBox.\n"; 
             } else {
-                warn "Failed to add interface: ", $post_response->status_line, "\n";
-                print "Response content: ", $post_response->decoded_content, "\n";
+                $error_output .= "Failed to add interface: " . $post_response->status_line . "\n";
+                $error++;
             }
         }
     }
@@ -634,14 +630,15 @@ sub compare_interfaces {
         }
 
         unless ($found_match) {
-            if($debug) { print "\t\tInterface $netbox_if->{name} not found in new interfaces. Removing from NetBox...\n"; }
-            #my $delete_response = $netbox_ua->delete("$netbox_api_url$netbox_virt_int_url$netbox_if_id");
+            $error_output .= "\t\tInterface $netbox_if->{name} not found in new interfaces. Removing from NetBox...\n";
+            my $delete_response = $netbox_ua->delete("$netbox_api_url$netbox_virt_int_url$netbox_if_id");
 
-            #if ($delete_response->is_success) {
-            #    print "Interface $netbox_if->{name} successfully removed from NetBox.\n";
-            #} else {
-            #    warn "Failed to delete interface: ", $delete_response->status_line, "\n";
-            #}
+            if ($delete_response->is_success) {
+                $error_output .= "Interface $netbox_if->{name} successfully removed from NetBox.\n";
+            } else {
+                $error_output .= "Failed to delete interface: " . $delete_response->status_line . "\n";
+                $error++;
+            }
         }
     }
 }
@@ -652,7 +649,8 @@ sub vm_ip_int {
 
     my $vm_int = get_vm_int($vm_name,$iface);
     if(!defined($vm_int)) { 
-        warn "No valid interface found\n"; 
+        $error_output .= "No valid interface found\n"; 
+        $error++;
         return; 
     }
 
@@ -669,29 +667,33 @@ sub vm_ip_int {
         } elsif($ipaddr =~ /:/) {
             $netmask = '128';
         } else {
-            print "\t\tnot sure on the netmask, moving on : $ipaddr\n";
+            $error_output .= "\t\tnot sure on the netmask, moving on : $ipaddr\n";
+            $error++;
             return;
         }
     }
 
     if (is_valid_ipv4($ipaddr)) {
         if ($netmask < 1 || $netmask > 32) {
-            print "Invalid IPv4 netmask: $netmask\n";
+            $error_output .= "Invalid IPv4 netmask: $netmask\n";
+            $error++;
             return;
         }
         $networkaddr = $ipaddr . "/" . $netmask;
     } elsif (is_valid_ipv6($ipaddr)) {
         if ($netmask < 1 || $netmask > 128) {
-            print "Invalid IPv6 netmask: $netmask\n";
+            $error_output .= "Invalid IPv6 netmask: $netmask\n";
+            $error++;
             return;
         }
         $networkaddr = $ipaddr . "/" . $netmask;
     } else {
-        print "\n\n\nIP is invalid, please audit this and try again: $ipaddr\n\n\n";
+        $error_output .= "\n\n\nIP is invalid, please audit this and try again: $ipaddr\n\n\n";
+        $error++;
         return;
     }
 
-    if($debug) {  print "\t\tVM interface : $vm_int, IP address : $networkaddr\n"; }
+    $error_output .= "\t\tVM interface : $vm_int, IP address : $networkaddr\n"; 
         
     my $netbox_ip_addresses_url = "ipam/ip-addresses/";
 
@@ -710,8 +712,8 @@ sub vm_ip_int {
         };
     }
 
-    if (!exists($netbox_addr{$networkaddr})) {
-        if($debug) { print "\t\t\tAdding address $networkaddr to NetBox...\n"; }
+    if (!grep { index($_, $ipaddr) != -1 } keys %netbox_addr) {
+        $error_output .= "\t\t\tAdding address $networkaddr to NetBox...\n";
         
         my $insert_payload = {
             address             => $networkaddr,
@@ -726,12 +728,13 @@ sub vm_ip_int {
         );
 
         if ($post_response->is_success) {
-            if($debug) { print "\t\t\t\tAdded IP $networkaddr to $vm_int in NetBox.\n"; }
+            $error_output .= "\t\t\t\tAdded IP $networkaddr to $vm_int in NetBox.\n";
         } else {
-            warn "Failed to add IP $networkaddr on $vm_name interface $vm_int: ", $post_response->status_line, "\n";
+            $error_output .= "Failed to add IP $networkaddr on $vm_name interface $vm_int: " . $post_response->status_line . "\n";
+            $error++;
         }
     } else {
-        if($debug) { print "\t\t\tIP already exists in the DB.\n"; }
+        $error_output .= "\t\t\tIP already exists in the DB.\n"; 
     }
 }
 
@@ -780,7 +783,7 @@ sub HelpMessage {
   print "\n";
   print "VERSION\n";
   print "\n";
-  print "1.04 - 3/14/2025\n";
+  print "1.05 - 3/19/2025\n";
   print "\n";
   exit;
 }
